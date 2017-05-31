420a13dfdbeb81252d6cdc9062ac27c3a19e5705
Марина Звягина <zvyagina.marina@gmail.com>
2017-05-31 19:32:45 +0300

    Initial Commit

---
+++ FlickrDownloadApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrDownloadApplication
+//
+//  Created by Марина Звягина on 31.05.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrDownloadApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrDownloadApplication
+//
+//  Created by Марина Звягина on 31.05.17.
+//  Copyright © 2017 Zvyagina Marina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
